fbb3df4701e555c4851815ea0f16d9cc61c103b5
Dylan Vidas <viddylan@gmail.com>
2021-04-08 06:58:39 -0500

    Changed control method from keyboard to mouse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 0.4f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,30 +15,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
-        {            
-            MovePaddle(-1);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
-        {
-            MovePaddle(1);
-        }
+        //if (Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
+        //{            
+        //    MovePaddle(-1);
+        //}
+        //else if (Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
+        //{
+        //    MovePaddle(1);
+        //}
+
+        float mouseSpeed = Input.GetAxis("Mouse X") * speed;
+        transform.Translate(mouseSpeed, 0, 0);
     }
 
     void MovePaddle(int direction) 
     {
-        switch (direction)
-        {
-            case 1:
-                //move right
-                transform.Translate(Vector3.right * speed * Time.deltaTime);
-                break;
-            case -1:
-                // move left
-                transform.Translate(Vector3.left * speed * Time.deltaTime);
-                break;
-            default:
-                break;
-        }
+        //switch (direction)
+        //{
+        //    case 1:
+        //        //move right
+        //        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        //        break;
+        //    case -1:
+        //        // move left
+        //        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //        break;
+        //    default:
+        //        break;
+        //}
     }
 }
