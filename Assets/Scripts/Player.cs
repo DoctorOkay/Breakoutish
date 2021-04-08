ff79470f47e19fcd9fde9450430261c0d9b2079a
Dylan Vidas <viddylan@gmail.com>
2021-04-07 20:33:32 -0500

    Got a paddle moving and a ball bouncing

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
+        {            
+            MovePaddle(-1);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
+        {
+            MovePaddle(1);
+        }
+    }
+
+    void MovePaddle(int direction) 
+    {
+        switch (direction)
+        {
+            case 1:
+                //move right
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                break;
+            case -1:
+                // move left
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+                break;
+            default:
+                break;
+        }
+    }
+}
